8af6730c3f5f6ff586011b436c5ff7abfb2962c0
earthloversclub <earthloversclub.jp@gmail.com>
2026-07-06 12:48:28 +0900

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -10,7 +10,7 @@ const medals = ["🥇", "🥈", "🥉"];
 
 const top3 = ranking.slice(0, 3)
 .map((item, index) =>
-'<div style="display:grid; grid-template-columns: 30px 1fr 65px; align-items:center; width:100%; white-space:nowrap;">' +
+'<div style="display:grid; grid-template-columns:30px 1fr 65px; align-items:center; width:100%; white-space:nowrap;">' +
 '<b style="text-align:center;">' + medals[index] + '</b>' +
 '<b style="text-align:left;">' + item.pref + '</b>' +
 '<b style="text-align:center;">' + item.thanks + '輪</b>' +
@@ -26,7 +26,8 @@ const nearFull = ranking.slice(3, 5)
 )
 .join("");
 
-document.getElementById("rankingBox").innerHTML =
+/* ===== スマホ版（現状維持） ===== */
+const rankingHtmlMobile =
 
 '<div style="margin-top:0px;">' +
 top3 +
@@ -37,4 +38,28 @@ top3 +
 '<div style="text-align:center; line-height:1.35; position:relative; top:-23px;">' +
 nearFull +
 '</div>';
+
+/* ===== PC版 ===== */
+const rankingHtmlPc =
+
+'<div class="rankingTitle">🌸満開ランキング🌸</div>' +
+
+'<div style="margin-top:0px;">' +
+top3 +
+'</div>' +
+
+'<hr class="rankingDivider">' +
+
+'<div class="rankingTitle">⭐注目の県⭐</div>' +
+
+'<div style="text-align:center; line-height:1.35;">' +
+nearFull +
+'</div>';
+
+/* ===== 表示切替 ===== */
+if (window.innerWidth >= 768) {
+document.getElementById("rankingBox").innerHTML = rankingHtmlPc;
+} else {
+document.getElementById("rankingBox").innerHTML = rankingHtmlMobile;
+}
 }
